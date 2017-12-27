fa7531267b3c39748e0a2bd6b5347237b0ed43ac
王涛 <maple216@126.com>
2017-12-27 09:17:55 +0800

    Initial Commit

---
+++ MHTLabelPrinter/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MHTLabelPrinter
+//
+//  Created by SiliconZou on 2017/12/26.
+//  Copyright © 2017年 MHT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
